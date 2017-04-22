d014f048a29208ce2a499ba009f5bd1d8aeaa291
ghst659 <ghst659@github.com>
2017-04-21 22:15:22 -0700

    Added strict mode.

---
+++ bin/cli.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import sys                      # access to basic things like sys.argv
+import os                       # access pathname utilities
+import argparse                 # for command-line options parsing
+import logging
+import fileinput
+import tc.fsm
+##############################################################################
+class ParseState:
+    def __init__(self, shared):
+        self._accum = shared
+    def clear(self):
+        self._accum.clear()
+    def close(self):
+        result = list(x.rstrip() for x in self._accum)
+        self.clear()
+        return result
+    def add(self, line):
+        self._accum.append(line)
+
+class Outside(ParseState):
+    def process(self, line):
+        # print("outside: %s" % line, file=sys.stderr)
+        next_state = self.name
+        result = None
+        if line.strip() != "":
+            self.add(line)
+            next_state = "Inside"
+            return next_state, result
+
+class Inside(ParseState):
+    def process(self, line):
+        # print("inside: %s" % line, file=sys.stderr)
+        next_state = self.name
+        if line.strip() == "":
+            result = self.close()
+            next_state = "Outside"
+        else:
+            result = None
+            self.add(line)
+        return next_state, result
+
+
+class LineParser:
+    def __init__(self):
+        self._buffer = []
+        self._fsm = tc.fsm.Context()
+        self._fsm.register_state(Outside(self._buffer))
+        self._fsm.register_state(Inside(self._buffer))
+        self._fsm.current_state = "Outside"
+        self._cleanup = ParseState(self._buffer)
+
+    def run(self, stream):
+        for line in stream:
+            # print("run: %s" % line, file=sys.stderr)
+            result = self._fsm.process(line)
+            if result:
+                yield result
+        conclusion = self._cleanup.close()
+        if conclusion:
+            yield conclusion
+##############################################################################
+def main(argv):
+    """
+    Example command-line entrypoint
+    """
+    exit_code = 0
+    global me; me = os.path.basename(argv[0]) # name of this program
+    global mydir; mydir = os.path.dirname(os.path.abspath(__file__))
+    ################################
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument("-l","--logfile", default=None,
+                        dest='logfile', 
+                        help="optional name of file for logging messages.")
+    parser.add_argument("-v","--verbose",
+                        dest='verbose', action="store_true",
+                        help="run verbosely")
+    parser.add_argument("targets",
+                        metavar="TARGET_FILE", nargs="*",
+                        help="target files to process, nargs=* means 0 or more")
+    args = parser.parse_args(args=argv[1:])  # will exit on parse error
+    ################################################################
+    # set up logging channels
+    log = logging.getLogger(me);
+    log.setLevel(logging.DEBUG if args.verbose else logging.INFO)
+    logfile = args.logfile
+    if isinstance(logfile,str):
+        if os.path.exists(logfile):
+            os.remove(logfile)
+        log.addHandler(logging.FileHandler(logfile))
+    console = logging.StreamHandler(sys.stderr); console.setLevel(logging.WARNING); log.addHandler(console)
+    ################################################################
+    if exit_code == 0:
+        buf = []
+        o = Outside(buf)
+        i = Inside(buf)
+        print("o.name = %s, i.name = %s" % (o.name, i.name))
+        lp = LineParser()
+        for stanza in lp.run(fileinput.input(args.targets)):
+            print("\n".join(stanza))
+            print("-" * 80)
+    ################################################################
+    return exit_code
+
+##############################################################################
+# The following code calls main only if this program is invoked standalone
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+##############################################################################
+# Text Editor Settings to help retain formatting; last line is for vi/vim
+# Local Variables:
+# mode: python
+# python-indent: 4
+# End:
+# vim: set expandtab:
--- tc/fsm.py
+++ tc/fsm.py
@@ -1,56 +1,11 @@
 #!/usr/bin/python3.2 -tt
 # Copyright (c) 2016 ghst659@github.com
 # All rights reserved.
+import collections
 import threading
 ##############################################################################
-class IllegalState(Exception):
-    """Signifies that an illegal state has been encountered."""
-    pass
-
-class Context:
-    """The state machine state engine."""
-
-    def __init__(self):
-        """Initiailses the state machine."""
-        self._lock = threading.RLock()
-        self._state_tbl = {}    # maps state name to register State objects
-        self._current = None    # current state of the machine
-
-    @property
-    def current_state(self):
-        """The name of the current machine state."""
-        result = None
-        with self._lock:
-            if self._current is not None:
-                result = self._current.name
-        return result
-
-    @current_state.setter
-    def current_state(self, state_tag):
-        """Sets current state to the state named STATE_TAG."""
-        with self._lock:
-            if state_tag in self._state_tbl:
-                self._current = self._state_tbl[state_tag]
-            else:
-                raise IllegalState("invalid state: %s" % state_tag)
-
-    def register_state(self, state_obj):
-        """Registers STATE_OBJECT as a possible state."""
-        tag = state_obj.name
-        with self._lock:
-            self._state_tbl[tag] = state_obj
-        return tag
-
-    def process(self, *args, **kwargs):
-        """Dispatch current state to process *ARGS and **KWARGS."""
-        process_result = None
-        with self._lock:
-            if self._current is not None:
-                next_state, process_result = self._current.process(*args, **kwargs)
-                self.current_state = next_state
-        return process_result
-
-
+ProcessResult = collections.namedtuple("ProcessResult", ["next", "data"])
+                                       
 class State:
     """Base class for a state in the state machine."""
     
@@ -63,7 +18,71 @@ class State:
         """Responds to *ARGS and **KWARGS, returning next state and result."""
         next_state_tag = self.name
         process_result = None
-        return (next_state_tag, process_result)
+        return ProcessResult(next=next_state_tag, data=process_result)
+
+class Context:
+    """The state machine state engine."""
+
+    def __init__(self):
+        """Initiailses the state machine."""
+        self._lock = threading.RLock()
+        self._state_tbl = {}    # maps state name to register State objects
+        self._current = None    # current state of the machine
+        self._strict = False    # raise ValueError on illegal state
+
+    @property
+    def strict(self):
+        """Return the state of the strict flag."""
+        return self._strict
+
+    @strict.setter
+    def strict(self, strictness):
+        """Sets the strict property to STRICTNESS."""
+        self._strict = strictness
+
+    @property
+    def current_state(self):
+        """The name of the current machine state."""
+        result = None
+        with self._lock:
+            if self._current is not None:
+                result = self._current.name
+            elif self._strict:
+                raise ValueError("illegal state")
+            else:
+                pass
+        return result
+
+    @current_state.setter
+    def current_state(self, state_tag):
+        """Sets current state to the state named STATE_TAG."""
+        with self._lock:
+            if state_tag in self._state_tbl:
+                self._current = self._state_tbl[state_tag]
+            elif self._strict:
+                raise ValueError("illegal next state: %s" % state_tag)
+            else:
+                pass
+
+    def register_state(self, state_obj):
+        """Registers STATE_OBJECT as a possible state."""
+        tag = state_obj.name    # may raise AttributeError
+        with self._lock:
+            self._state_tbl[tag] = state_obj
+        return tag
+
+    def process(self, *args, **kwargs):
+        """Dispatch current state to process *ARGS and **KWARGS."""
+        process_result = None
+        with self._lock:
+            if self._current is not None:
+                next_state, process_result = self._current.process(*args, **kwargs)
+                self.current_state = next_state
+            elif self._strict:
+                raise ValueError("illegal state")
+            else:
+                pass
+        return process_result
 
 # Local Variables:
 # mode: python
